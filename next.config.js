a0953ce187c29f90422fe589cde2092929cb5b22
Andres Eduardo Diaz Suancha <andres.diaz@usc.com.co>
2024-11-28 14:07:55 -0500

    vault backup: 2024-11-28 14:07:55

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ module.exports = {
     webpack: (config) => {
         config.resolve.fallback = {
             fs: false,
-            buffer: false
+            buffer: false,
         };
         return config;
     },
-}
+    staticPageGenerationTimeout: 300, // Aumenta el tiempo de espera a 120 segundos
+};
